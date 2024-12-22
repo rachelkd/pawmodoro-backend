b2b78d69636554a92083bd48c11e4351aeaf6c34
rachelkd <rachel05deng@gmail.com>
2024-12-21 20:11:35 -0800

    Added GetAllCatsResponseDto back

---
+++ src/main/java/com/pawmodoro/cats/service/get_all_cats/interface_adapter/GetAllCatsResponseDto.java
@@ -0,0 +1,19 @@
+package com.pawmodoro.cats.service.get_all_cats.interface_adapter;
+
+import java.util.Collection;
+
+import com.pawmodoro.cats.entity.Cat;
+
+/**
+ * Data Transfer Object for get all cats response.
+ * This record encapsulates the data that will be sent back to the client.
+ * @param success whether the operation was successful
+ * @param cats collection of cats (null if operation failed)
+ * @param message success or error message
+ */
+public record GetAllCatsResponseDto(
+    boolean success,
+    Collection<Cat> cats,
+    String message) {
+
+}
